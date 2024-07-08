184fa9cbb83aa39fc333ed678b565b9884204653
tzuhuangyen <912yan@gmail.com>
2024-07-08 20:08:19 +0200

    navbar

---
+++ client/src/pages/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Nav,
+  NavDropdown,
+  Dropdown,
+} from 'react-bootstrap';
+import userAvatar from '/image/user.png';
+const Navbar = (props) => {
+  return (
+    <div>
+      {' '}
+      <Nav className='border-bottom border-dark'>
+        <Container fluid='lg'>
+          <Nav className=' d-flex justify-content-between '>
+            <h1 className='logo'>MetaWall</h1>
+            <div className='member d-flex  align-items-center'>
+              <img src={userAvatar} alt='userAvatar' />
+              <NavDropdown title='Dropdown' id='nav-dropdown'>
+                <NavDropdown.Item eventKey='4.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item eventKey='4.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey='4.3'>
+                  Something else here
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item eventKey='4.4'>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>{' '}
+            </div>
+          </Nav>
+        </Container>
+      </Nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- client/src/pages/PostsWall.jsx
+++ client/src/pages/PostsWall.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
-import { Container, Row, Col, Nav, NavDropdown } from 'react-bootstrap';
-import userAvatar from '/image/user.png';
+import Navbar from './Navbar';
+import PostsWall from './PostsWall';
 const PostsWall = () => {
   return (
     <>
-      <Nav className='border-bottom border-dark'>
-        <Container fluid='lg'>
-          <Nav className=' d-flex justify-content-between '>
-            <h1 className='logo'>MetaWall</h1>
-            <div className='member d-flex  align-items-center'>
-              <img src={userAvatar} alt='userAvatar' />
-              <NavDropdown title='Dropdown' id='nav-dropdown'>
-                <NavDropdown.Item eventKey='4.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item eventKey='4.2'>
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item eventKey='4.3'>
-                  Something else here
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item eventKey='4.4'>
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>{' '}
-            </div>
-          </Nav>
-        </Container>
-      </Nav>
+      <Navbar />
+      <PostsWall />
     </>
   );
 };
