6568b2c2cb54c9c401ea28ed63da052712435f0e
tzuhuangyen <912yan@gmail.com>
2024-08-22 18:28:35 +0200

    uploadImg

--- server/controllers/uploadControllers.js
+++ server/controllers/uploadControllers.js
@@ -5,12 +5,16 @@ const appError = require('../service/appError');
 const handleErrorAsync = require('../service/handleErrorAsync');
 
 const upload = multer({
+  //容量限制
   storage: multer.memoryStorage(),
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter(req, file, cb) {
+    console.log(file);
+    // 生成唯一的文件名
     const ext = path.extname(file.originalname).toLowerCase();
+    //限定檔案格式
     if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
       cb(
         new Error('file format is not supported,only jpg, png, jpeg is allowed')
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,15 +11,14 @@ const Validator = require('validator');
 const userModel = require('../models/userModel');
 const { generateJWT, isAuth } = require('../service/auth');
 //upload image
-// const sizeOf = require('image-size');
+
 const upload = require('../controllers/uploadControllers');
-// const { ImgurClient } = require('imgur');
 const { v4: uuidv4 } = require('uuid');
 const firebaseAdmin = require('../connections/firebase');
-const uploadControllers = require('../controllers/uploadControllers');
+const bucket = firebaseAdmin.storage().bucket();
+
 const PostModel = require('../models/postModel');
 const UserModel = require('../models/userModel');
-const bucket = firebaseAdmin.storage().bucket();
 //app.use('/api/users', userRoutes);
 //v sign up v
 router.post(
@@ -58,7 +57,7 @@ router.post(
     generateJWT(newUser, 201, res);
   })
 );
-//v login  / sign inv
+//v login  / sign in v
 router.post(
   '/login',
   handleErrorAsync(async (req, res, next) => {
@@ -131,41 +130,44 @@ router.patch(
   })
 );
 //image
-//get image v
-router.get(
-  '/image',
-  handleErrorAsync(async (req, res) => {
-    const [files] = await bucket.getFiles();
+//get image
+// router.get(
+//   '/image',
+//   handleErrorAsync(async (req, res) => {
+//     const [files] = await bucket.getFiles();
 
-    const fileList = await Promise.all(
-      files.map(async (file) => {
-        const [fileUrl] = await file.getSignedUrl({
-          action: 'read',
-          expires: '03-09-2491',
-        });
-        return {
-          fileName: file.name,
-          imgUrl: fileUrl,
-        };
-      })
-    );
+//     const fileList = await Promise.all(
+//       files.map(async (file) => {
+//         const [fileUrl] = await file.getSignedUrl({
+//           action: 'read',
+//           expires: '03-09-2491',
+//         });
+//         return {
+//           fileName: file.name,
+//           imgUrl: fileUrl,
+//         };
+//       })
+//     );
 
-    res.send(fileList);
-  })
-);
+//     res.send(fileList);
+//   })
+// );
 
-//upload image v
+// v upload image v
 router.post(
   '/upload/image',
-  // isAuth,
+  isAuth,
   upload,
   handleErrorAsync(async (req, res, next) => {
     if (!req.file) {
       return next(appError(400, 'please upload image', next));
     }
+    if (!req.user || !req.user._id) {
+      return next(appError(400, 'User not authenticated', next));
+    }
     //取得用戶上傳的第一個檔案
     const file = req.file;
-    //基於檔案的原始名稱建立一個blob物件
+    //基於檔案的原始名稱建立一個blob物件 生成新的文件名，防止文件名冲突
     const blob = bucket.file(
       `images/${uuidv4()}.${file.originalname.split('.').pop()}`
     );
@@ -173,18 +175,29 @@ router.post(
     const blobStream = blob.createWriteStream();
     // 監聽上傳狀態，當上傳完成時，會觸發 finish 事件
     blobStream.on('finish', () => {
+      console.log('Upload finished, generating signed URL...');
+
       // 設定檔案的存取權限
       const config = {
         action: 'read', // 權限
         expires: '12-31-2500', // 網址的有效期限
       };
       // 取得檔案的網址
-      blob.getSignedUrl(config, (err, imgUrl) => {
+      blob.getSignedUrl(config, async (err, imgUrl) => {
         if (err) {
+          console.error('Error generating signed URL:', err);
+
           return res.status(500).send(err.message);
         }
+        console.log('Generated signed URL:', imgUrl);
+
+        // 這裡將圖片URL保存到MongoDB中
+        const userId = req.user._id; // 假設已經進行身份驗證
+        await UserModel.findByIdAndUpdate(userId, {
+          photo: imgUrl,
+        });
         res.send({
-          imgUrl,
+          imgUrl: imgUrl,
         });
       });
     });
@@ -197,21 +210,21 @@ router.post(
   })
 );
 //delete image
-router.delete('/delete/image', (req, res) => {
-  // 取得檔案名稱
-  const fileName = req.query.fileName;
-  // 取得檔案
-  const blob = bucket.file(fileName);
-  // 刪除檔案
-  blob
-    .delete()
-    .then(() => {
-      res.send('delete success');
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
-});
+// router.delete('/delete/image', (req, res) => {
+//   // 取得檔案名稱
+//   const fileName = req.query.fileName;
+//   // 取得檔案
+//   const blob = bucket.file(fileName);
+//   // 刪除檔案
+//   blob
+//     .delete()
+//     .then(() => {
+//       res.send('delete success');
+//     })
+//     .catch((err) => {
+//       res.status(500).send(err.message);
+//     });
+// });
 
 //get / likes list 取得個人按其他人讚列表v
 router.get(
